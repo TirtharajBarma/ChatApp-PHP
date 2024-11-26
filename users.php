70b28e89760bf8b239577d4b261c6fd1b289444e
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2024-11-26 22:26:25 +0530

    users list in dashboard

--- users.php
+++ users.php
@@ -1,30 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Realtime chat App</title>
-    <link rel="stylesheet" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    />
-  </head>
+<?php
+  session_start();
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
   <body>
     <div class="wrapper">
       <section class="users">
         <header>
+
+        <?php
+        include_once "php/config.php";
+          $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+          if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+          }
+        ?>
+
+        <!-- fname, lname, img changing using php -->
+
           <div class="content">
             <img
-              src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
+              src="php/images/<?php echo $row['img'] ?>"
               alt=""
             />
             <div class="details">
-              <span>Tirtharaj Barma</span>
-              <p>Active now</p>
+              <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
+              <p><?php echo $row['status'] ?></p>
             </div>
           </div>
-          <a href="" class="logout">Logout</a>
+          <a href="php/logout.php?logout_id=<?php echo $row['unique_id'] ?>" class="logout">Logout</a>
         </header>
         <div class="search">
           <span class="text">Select an user to start chat</span>
@@ -37,118 +44,9 @@
           <button><i class="fas fa-search"></i></button>
         </div>
         <div class="users-list">
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img
-                src="https://i.pinimg.com/736x/57/3a/46/573a46c7818f8cca76e394ac5af72542.jpg"
-                alt=""
-              />
-
-              <div class="details">
-                <span>Tirtharaj Barma</span>
-                <p>This is a test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fas fa-circle"></i></div>
-          </a>
+         
+        <!-- display dynamically users -->
+          
         </div>
       </section>
     </div>
